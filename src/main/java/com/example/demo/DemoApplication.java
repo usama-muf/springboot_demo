450738539390ed76ee4133d8b857196271f1cef0
Mohammed Usama <60036208+usama-muf@users.noreply.github.com>
2023-03-12 07:16:36 +0000

    added some endpoints

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+
+	@GetMapping
+	public String message() {
+		return "Welcome to demo SpringBoot Project";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
